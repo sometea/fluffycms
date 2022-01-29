370b4fddcf1861bc365e7416589cfe9c70fe3ee7
David Dasenbrook <david.dasenbrook@tngtech.com>
2022-01-29 19:14:47 +0100

    add react-admin and react-admin-firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Admin, Resource } from "react-admin";
+import {
+  FirebaseDataProvider,
+  FirebaseAuthProvider,
+  RAFirebaseOptions,
+} from "react-admin-firebase";
+import { firebaseConfig } from "./firebaseConfig";
+import { PostCreate, PostEdit, PostList, PostShow } from "./posts";
+
+
+const options: RAFirebaseOptions = {
+  logging: true,
+  rootRef: "root_collection/posts",
+};
+
+const dataProvider = FirebaseDataProvider(firebaseConfig, options);
+const authProvider = FirebaseAuthProvider(firebaseConfig, options);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Admin dataProvider={dataProvider} authProvider={authProvider}>
+    <Resource name="posts" list={PostList} create={PostCreate} show={PostShow} edit={PostEdit} />
+  </Admin>;
 }
 
 export default App;
---
+++ src/firebaseConfig.template.ts
@@ -0,0 +1,10 @@
+// copy this to firebaseConfig.ts and replace with your own firebase project values
+
+export const firebaseConfig = {
+  apiKey: "AAA",
+  authDomain: "BBB",
+  projectId: "CCC",
+  storageBucket: "DDD",
+  messagingSenderId: "EEE",
+  appId: "FFF"
+};
