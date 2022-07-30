7ca9a736ee78a04241fa75891881f15cea3b53e2
David Dasenbrook <david.dasenbrook@tngtech.com>
2022-07-30 16:10:07 +0200

    implement get endpoint and optimise list endpoint

--- functions/src/api.ts
+++ functions/src/api.ts
@@ -2,21 +2,25 @@ import express from "express";
 
 export function getApiApp(firestore: FirebaseFirestore.Firestore) {
   const apiApp = express();
+  const collection = firestore.collection('root_collection/fluffy/posts');
 
   apiApp.get('/', async (req, res) => {
-    const collection = firestore.collection('root_collection/fluffy/posts');
-    const posts = await collection.listDocuments();
-    const resolvedPosts = await Promise.all(posts.map(async post => (await post.get()).data()));
+    const posts = await collection.get();
+    const mappedPosts = posts.docs
+      .map(post => post.data())
+      .map(post => ({
+        id: post.id,
+        title: post.title,
+    }));
     res.send({
       message: 'hello',
-      documents: resolvedPosts,
+      documents: mappedPosts,
     });
   });
 
-  apiApp.get('/:id', (req, res) => {
-    res.send({
-      message: `id requested: ${req.params.id}`,
-    });
+  apiApp.get('/:id', async (req, res) => {
+    const post = await collection.doc(req.params.id).get();
+    res.send(post.data());
   });
 
   return apiApp;
