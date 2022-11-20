f7518eda352c859de9ff107aed4b0c3c3238a1cd
David Dasenbrook <david.dasenbrook@tngtech.com>
2022-11-20 15:06:39 +0100

    allow querying api by slug

--- functions/src/api.ts
+++ functions/src/api.ts
@@ -12,21 +12,41 @@ export function getApiApp(firestore: FirebaseFirestore.Firestore): ReturnType<ty
     return res.status(401).send();
   });
 
+  function mapPost(dbPost: FirebaseFirestore.DocumentData) {
+    return {
+      id: dbPost.id,
+      slug: dbPost.slug,
+      title: dbPost.title,
+      teaser: dbPost.teaser || '',
+      date: (dbPost.lastupdate?.['_seconds'] || 0) * 1000,
+      image: (dbPost.pictures?.length || 0) > 0 ? dbPost.pictures[0].src : '',
+    }
+  }
+
   apiApp.get('/', async (req, res) => {
+    if (typeof req.query['slug'] === 'string') {
+      const post = await getBySlug(req.query['slug']);
+      if (post === null) {
+        return res.sendStatus(404);
+      }
+      return res.send(post);
+    }
     const posts = await collection.get();
     const mappedPosts = posts.docs
       .map(post => post.data())
-      .map(post => ({
-        id: post.id,
-        title: post.title,
-        teaser: post.teaser || '',
-        date: (post.lastupdate?.['_seconds'] || 0) * 1000,
-        image: (post.pictures?.length || 0) > 0 ? post.pictures[0].src : '',
-      }));
-    res.send({
+      .map(mapPost);
+    return res.send({
       documents: mappedPosts,
     });
   });
+
+  async function getBySlug(slug: string) {
+    const foundPosts = (await collection.where('slug', '==', slug).get()).docs;
+    if (foundPosts.length === 0) {
+      return null;
+    }
+    return foundPosts[0].data();
+  }
 
   apiApp.get('/:id', async (req, res) => {
     const post = await collection.doc(req.params.id).get();
